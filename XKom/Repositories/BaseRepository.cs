a16c3f4160bbb58569699d94d77f2c493364b43b
Shimakaze-Kan <wiktormd@outlook.com>
2021-05-23 00:13:16 +0200

    Add base repostiory class

---
+++ XKom/Repositories/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XKom.Models.ModelsDB;
+
+namespace XKom.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly XKomContext _xKomContext;
+
+        public BaseRepository(XKomContext xKomContext)
+        {
+            _xKomContext = xKomContext;
+        }
+    }
+}
