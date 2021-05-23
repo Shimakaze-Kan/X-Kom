73770de59739377423bf335349ba0bb4554f0388
Shimakaze-Kan <wiktormd@outlook.com>
2021-05-23 13:29:06 +0200

    Document repository

--- XKom/Repositories/MeetingRepository.cs
+++ XKom/Repositories/MeetingRepository.cs
@@ -19,8 +19,10 @@ namespace XKom.Repositories
             using var transaction = _xKomContext.Database.BeginTransaction();
             try
             {
+                //Check if the participant has used the system before
                 var previousParticipant = await _xKomContext.Participants.SingleOrDefaultAsync(x => x.Email == participant.Email);
 
+                //If not, add them to the database
                 if (previousParticipant is null)
                 {
                     previousParticipant = new Participant()
@@ -35,6 +37,7 @@ namespace XKom.Repositories
                 }
                 else
                 {
+                    //If the given email exists in the database, check that the given name also matches
                     if (previousParticipant.Name != participant.Name)
                     {
                         transaction.Rollback();
@@ -46,9 +49,11 @@ namespace XKom.Repositories
                     }
                 }
 
+                //Check if the participant is already signed up for the requested meeting
                 var previousAssigment = await _xKomContext.MeetingsParticipants.SingleOrDefaultAsync(x 
                     => x.MeetingId == participant.MeetingId && x.ParticipantId == previousParticipant.ParticipantId);
 
+                //If so, return an error message
                 if (previousAssigment is not null)
                 {
                     transaction.Rollback();
@@ -61,12 +66,14 @@ namespace XKom.Repositories
 
                 await _xKomContext.SaveChangesAsync();
 
+                //Add a participant to a meeting
                 await _xKomContext.MeetingsParticipants.AddAsync(new() { MeetingId = participant.MeetingId, ParticipantId = previousParticipant.ParticipantId });
 
                 await _xKomContext.SaveChangesAsync();
 
                 var count = _xKomContext.MeetingsParticipants.Count(x => x.MeetingId == participant.MeetingId);
 
+                //Check if the participant is an redundant participant
                 if (count > 25)
                 {
                     transaction.Rollback();
@@ -91,6 +98,7 @@ namespace XKom.Repositories
 
         public async Task<MeetingResponseDto> CreateMeeting(MeetingRequestDto meetingRequest)
         {
+            //Check if the given meeting type name exists in the database
             var meetingType = await _xKomContext.Meetingtypes.Where(x => x.TypeName == meetingRequest.MeetingType).FirstOrDefaultAsync();
 
             if (meetingType is null)
@@ -103,6 +111,7 @@ namespace XKom.Repositories
             }
             else
             {
+                //If so, create a meeting
                 Meeting meeting = new()
                 {
                     MeetingId = Guid.NewGuid(),
@@ -158,10 +167,12 @@ namespace XKom.Repositories
 
         public async Task<MessageResponseDto> RemoveMeeting(Guid meetingId)
         {
+            //Check if a meeting with this id exists
             var meetingToRemove = await _xKomContext.Meetings.SingleOrDefaultAsync(x => x.MeetingId == meetingId);
 
             if (meetingToRemove is not null)
             {
+                //If so, remove
                 _xKomContext.Remove(meetingToRemove);
 
                 await _xKomContext.SaveChangesAsync();
@@ -174,6 +185,7 @@ namespace XKom.Repositories
 
             return new()
             {
+                //If not, return an error message
                 ErrorMessage = "The meeting with the given id does not exist",
                 IsSuccess = false
             };
